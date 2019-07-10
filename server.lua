23717669d9abc97264798090f908a32b6483a7a4
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-07-10 19:09:57 +0300

    Update server.lua
    Fix Annon system

--- server.lua
+++ server.lua
@@ -1,7 +1,7 @@
 local realhash = "Please-Change"
 
 RegisterCommand("announce", function(source, args, raw)
-    -- if IsPlayerAceAllowed(source,"command")then
+    if realhash == hash then 
     local scaleType, time, font = tonumber(args[1]), tonumber(args[2]), args[3]
     local extraArgs
     if tonumber(args[3]) then
@@ -12,7 +12,7 @@ RegisterCommand("announce", function(source, args, raw)
     for i = 1, extraArgs do table.remove(args, 1) end
     TriggerClientEvent("Scaleform:Announce", -1, scaleType, time, font,
                        table.concat(args, " "))
-    -- end
+     end
 end, false)
 
 RegisterCommand("kickplayerforfxcontroller", function(source, args, raw)
